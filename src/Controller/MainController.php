4badca0bc7850982b4c336bd56e668b9e34e8ccf
Robine Courtois <robine.courtois2025@campus-eni.fr>
2025-05-26 17:33:18 +0200

    page d'accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\CampusRepository;
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
 {
     #[Route('/', name: 'main_home', methods: ['GET'])]
-    public function home(): Response
+    public function home(Request $request, SortieRepository $sortieRepository, CampusRepository $campusRepository): Response
     {
-        return $this->render('main/home.html.twig');
+        $user = $this->getUser();
+        $campusList = $campusRepository->findAll();
+
+        //todo : faire un tri par état
+        $sorties = $sortieRepository->findAll();
+
+
+        return $this->render('main/home.html.twig', [
+            'sorties' => $sorties,
+            'campusList' => $campusList,
+            'user' => $user,
+        ]);
     }
 }
