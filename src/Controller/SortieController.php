c1d4e2b2c275f18b162a5f77df7b7b93593fb96e
Robine Courtois <robine.courtois2025@campus-eni.fr>
2025-05-28 15:10:12 +0200

    mise en place de l'inscription et le désistement a une sortie + page détails non fini

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use App\Entity\Sortie;
 use App\Form\SortieForm;
 use App\Repository\SortieRepository;
 use App\Utils\Etat;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -40,4 +42,51 @@ final class SortieController extends AbstractController
 			'form' => $form,
 		]);
 	}
+    #[Route('/sortie/{id}/inscrire', name: 'sortie_inscrire')]
+    public function inscrire(Sortie $sortie, EntityManagerInterface $em, Security $security): Response
+    {
+        $participant = $security->getUser();
+
+        if (!$participant instanceof Participant) {
+            throw new \LogicException('Utilisateur non connecté ou invalide.');
+        }
+
+        if (!$sortie->getParticipants()->contains($participant)) {
+            $sortie->addParticipant($participant);
+            $em->flush();
+            $this->addFlash('success', '✅ Vous êtes bien inscrit(e) à la sortie.');
+        }
+
+        return $this->redirectToRoute('main_home');
+    }
+
+
+    #[Route('/sortie/{id}/desister', name: 'sortie_desister')]
+    public function desister(Sortie $sortie, EntityManagerInterface $em, Security $security): Response
+    {
+        $participant = $security->getUser();
+
+        if (!$participant instanceof Participant) {
+            throw new \LogicException('Utilisateur non connecté ou invalide.');
+        }
+
+        if ($sortie->getParticipants()->contains($participant)) {
+            $sortie->removeParticipant($participant);
+            $em->flush();
+            $this->addFlash('success', '⚠️ Vous vous êtes désisté(e) de la sortie.');
+        }
+
+        return $this->redirectToRoute('main_home');
+    }
+
+    #[Route('/sortie/{id}', name: 'sortie_details')]
+    public function details(Sortie $sortie): Response
+    {
+        return $this->render('sortie/details.html.twig', [
+            'sortie' => $sortie,
+        ]);
+    }
+
+
+
 }
