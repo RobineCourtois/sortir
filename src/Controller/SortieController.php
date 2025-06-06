e7dad9ab43fcb3fb98b2dcdcd08b1182f83a25a7
Robine Courtois <robine.courtois2025@campus-eni.fr>
2025-06-06 11:51:04 +0200

    annulation des sorties

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use App\Entity\Sortie;
+use App\Form\AnnulationSortieForm;
 use App\Form\SortieForm;
 use App\Repository\LieuRepository;
 use App\Repository\VilleRepository;
@@ -20,142 +21,143 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted("ROLE_PARTICIPANT")]
 final class SortieController extends AbstractController
 {
-	#[Route('/sortie/creer', name: 'sortie_creer')]
-	public function creerSortie(
-		Request $request,
-		EntityManagerInterface $em,
-		LieuRepository $lieuRepository,
-		VilleRepository $villeRepository,
-	): Response
-	{
-		$sortie = new Sortie();
-		$sortie->setOrganisateur($this->getUser());
-		$sortie->setSiteOrganisateur($this->getUser()->getCampus());
-		$sortie->setEtat(Etat::EN_CREATION);
+    #[Route('/sortie/creer', name: 'sortie_creer')]
+    public function creerSortie(
+        Request                $request,
+        EntityManagerInterface $em,
+        LieuRepository         $lieuRepository,
+        VilleRepository        $villeRepository,
+    ): Response
+    {
+        $sortie = new Sortie();
+        $sortie->setOrganisateur($this->getUser());
+        $sortie->setSiteOrganisateur($this->getUser()->getCampus());
+        $sortie->setEtat(Etat::EN_CREATION);
 
-		$lieux  = $lieuRepository->findAll();
-		$tabLieux = [];
-		foreach ($lieux as $lieu){
-			$tabLieux[$lieu->getId()] = $lieu;
-		}
+        $lieux = $lieuRepository->findAll();
+        $tabLieux = [];
+        foreach ($lieux as $lieu) {
+            $tabLieux[$lieu->getId()] = $lieu;
+        }
 
-		$villes = $villeRepository->findAll();
+        $villes = $villeRepository->findAll();
 
-		$form = $this->createForm(SortieForm::class, $sortie);
-		$form->handleRequest($request);
+        $form = $this->createForm(SortieForm::class, $sortie);
+        $form->handleRequest($request);
 
-		if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
 
-			/** @var UploadedFile $image */
-			$imageFile = $form->get('image')->getData();
-			if ($imageFile) {
-				$newFileName = uniqid().'.'.$imageFile->guessExtension();
-				try {
-					$imageFile->move(
-						$this->getParameter('app.project_images_directory'),
-						$newFileName
-					);
-					$sortie->setFilename($newFileName);
-				} catch (FileException $e) {
-					$this->addFlash('error', "Le fichier n'a pas pu être enregistré");
-				}
-			}
+            /** @var UploadedFile $image */
+            $imageFile = $form->get('image')->getData();
+            if ($imageFile) {
+                $newFileName = uniqid() . '.' . $imageFile->guessExtension();
+                try {
+                    $imageFile->move(
+                        $this->getParameter('app.project_images_directory'),
+                        $newFileName
+                    );
+                    $sortie->setFilename($newFileName);
+                } catch (FileException $e) {
+                    $this->addFlash('error', "Le fichier n'a pas pu être enregistré");
+                }
+            }
 
-			if ($form->get('publier')->isClicked()){
-				$sortie->setEtat(Etat::OUVERTE);
-			}
-			$em->persist($sortie);
-			$em->flush();
+            if ($form->get('publier')->isClicked()) {
+                $sortie->setEtat(Etat::OUVERTE);
+            }
+            $em->persist($sortie);
+            $em->flush();
 
-			$this->addFlash("success", "sortie créée avec succès !");
-			return $this->redirectToRoute('main_home');
-		}
+            $this->addFlash("success", "sortie créée avec succès !");
+            return $this->redirectToRoute('main_home');
+        }
 
 
-		return $this->render('sortie/creer.html.twig', [
-			'lieux' => $tabLieux,
-			'form' => $form,
-			'villes' => $villes,
-		]);
-	}
+        return $this->render('sortie/creer.html.twig', [
+            'lieux' => $tabLieux,
+            'form' => $form,
+            'villes' => $villes,
+        ]);
+    }
 
-	#[Route('/sortie/{id}/modifier', name: 'sortie_modifier', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
-	public function modifierSortie(
-		Request $request,
-		Sortie $sortie,
-		EntityManagerInterface $em,
-		LieuRepository $lieuRepository,
-		VilleRepository $villeRepository,
-	): Response
-	{
+    #[Route('/sortie/{id}/modifier', name: 'sortie_modifier', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function modifierSortie(
+        Request                $request,
+        Sortie                 $sortie,
+        EntityManagerInterface $em,
+        LieuRepository         $lieuRepository,
+        VilleRepository        $villeRepository,
+    ): Response
+    {
 
-		if ($sortie->getOrganisateur() !== $this->getUser() and !$this->isGranted('ROLE_ADMIN')){
-			throw $this->createAccessDeniedException("Vous n'avez pas le droit de modifier cette sortie");
-		}
+        if ($sortie->getOrganisateur() !== $this->getUser() and !$this->isGranted('ROLE_ADMIN')) {
+            throw $this->createAccessDeniedException("Vous n'avez pas le droit de modifier cette sortie");
+        }
         if ($sortie->getEtat() === Etat::OUVERTE && !$this->isGranted('ROLE_ADMIN')) {
             throw $this->createAccessDeniedException("Une sortie ne peut pas être modifiée une fois publiée");
         }
 
 
-        $lieux  = $lieuRepository->findAll();
-		$tabLieux = [];
-		foreach ($lieux as $lieu){
-			$tabLieux[$lieu->getId()] = $lieu;
-		}
+        $lieux = $lieuRepository->findAll();
+        $tabLieux = [];
+        foreach ($lieux as $lieu) {
+            $tabLieux[$lieu->getId()] = $lieu;
+        }
 
-		$villes = $villeRepository->findAll();
+        $villes = $villeRepository->findAll();
 
-		$form = $this->createForm(SortieForm::class, $sortie);
-		$form->handleRequest($request);
+        $form = $this->createForm(SortieForm::class, $sortie);
+        $form->handleRequest($request);
 
-		if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
 
-			/** @var UploadedFile $image */
-			$imageFile = $form->get('image')->getData();
-			if ($imageFile) {
-				$newFileName = uniqid().'.'.$imageFile->guessExtension();
-				try {
-					$imageFile->move(
-						$this->getParameter('app.project_images_directory'),
-						$newFileName
-					);
-					$sortie->setFilename($newFileName);
-				} catch (FileException $e) {
-					$this->addFlash('error', "Le fichier n'a pas pu être enregistré");
-				}
-			}
+            /** @var UploadedFile $image */
+            $imageFile = $form->get('image')->getData();
+            if ($imageFile) {
+                $newFileName = uniqid() . '.' . $imageFile->guessExtension();
+                try {
+                    $imageFile->move(
+                        $this->getParameter('app.project_images_directory'),
+                        $newFileName
+                    );
+                    $sortie->setFilename($newFileName);
+                } catch (FileException $e) {
+                    $this->addFlash('error', "Le fichier n'a pas pu être enregistré");
+                }
+            }
 
-			if ($form->get('publier')->isClicked()){
-				$sortie->setEtat(Etat::OUVERTE);
-			}
-			$em->persist($sortie);
-			$em->flush();
+            if ($form->get('publier')->isClicked()) {
+                $sortie->setEtat(Etat::OUVERTE);
+            }
+            $em->persist($sortie);
+            $em->flush();
 
-			$this->addFlash("success", "Sortie modifiée avec succès !");
-			return $this->redirectToRoute('main_home');
-		}
+            $this->addFlash("success", "Sortie modifiée avec succès !");
+            return $this->redirectToRoute('main_home');
+        }
 
-		return $this->render('sortie/modifier.html.twig', [
-			'form' => $form,
-			'sortie' => $sortie,
-			'lieux' => $tabLieux,
-			'villes' => $villes,
-		]);
-	}
+        return $this->render('sortie/modifier.html.twig', [
+            'form' => $form,
+            'sortie' => $sortie,
+            'lieux' => $tabLieux,
+            'villes' => $villes,
+        ]);
+    }
 
-	#[Route('/sortie/{id}/supprimer/{token}', name: 'sortie_supprimer', requirements: ['id' => '\d+'], methods: ['GET'])]
-	public function supprimerSortie(Sortie $sortie, string $token, EntityManagerInterface $em): Response
-	{
-		if($this->isCsrfTokenValid('supprimer-sortie-'.$sortie->getId(), $token)){
-			$em->remove($sortie);
-			$em->flush();
-			$this->addFlash("success", "La sortie à été supprimée avec succès !");
-			return $this->redirectToRoute('main_home');
-		}
-		$this->addFlash("danger", "Une erreur est survenue lors de la suppression de la sortie !");
-		return $this->redirectToRoute('sortie_modifier', ['id' => $sortie->getId()]);
+    #[Route('/sortie/{id}/supprimer/{token}', name: 'sortie_supprimer', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function supprimerSortie(Sortie $sortie, string $token, EntityManagerInterface $em): Response
+    {
+        if ($this->isCsrfTokenValid('supprimer-sortie-' . $sortie->getId(), $token)) {
+            $em->remove($sortie);
+            $em->flush();
+            $this->addFlash("success", "La sortie à été supprimée avec succès !");
+            return $this->redirectToRoute('main_home');
+        }
+        $this->addFlash("danger", "Une erreur est survenue lors de la suppression de la sortie !");
+        return $this->redirectToRoute('sortie_modifier', ['id' => $sortie->getId()]);
 
-	}
+    }
+
     #[Route('/sortie/{id}', name: 'sortie_details', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function detail(Sortie $sortie): Response
     {
@@ -163,7 +165,6 @@ final class SortieController extends AbstractController
             'sortie' => $sortie,
         ]);
     }
-
 
 
     #[Route('/sortie/{id}/inscription', name: 'sortie_inscrire', methods: ['POST'])]
@@ -205,9 +206,9 @@ final class SortieController extends AbstractController
 
         //  Inscription OK
         $sortie->addParticipant($user);
-		if ($sortie->getNbInscriptionMax() == $sortie->getParticipants()->count()){
-			$sortie->setEtat(Etat::CLOTUREE);
-		}
+        if ($sortie->getNbInscriptionMax() == $sortie->getParticipants()->count()) {
+            $sortie->setEtat(Etat::CLOTUREE);
+        }
         $em->flush();
         $this->addFlash('success', 'Inscription réussie à la sortie !');
 
@@ -233,9 +234,9 @@ final class SortieController extends AbstractController
 
         // Désistement
         if ($sortie->getParticipants()->contains($user)) {
-			if ($sortie->getNbInscriptionMax() == $sortie->getParticipants()->count()){
-				$sortie->setEtat(Etat::OUVERTE);
-			}
+            if ($sortie->getNbInscriptionMax() == $sortie->getParticipants()->count()) {
+                $sortie->setEtat(Etat::OUVERTE);
+            }
             $sortie->removeParticipant($user);
             $em->flush();
             $this->addFlash('success', 'Vous vous êtes désisté(e) de la sortie.');
@@ -246,25 +247,41 @@ final class SortieController extends AbstractController
         return $this->redirectToRoute('main_home');
     }
 
-    #[Route('/sortie/{id}/annuler', name: 'sortie_annuler', methods: ['POST'])]
-    public function annuler(Sortie $sortie, Request $request, EntityManagerInterface $em): Response
+    // Affiche le formulaire pour saisir un motif d'annulation
+    #[Route('/sortie/{id}/annuler', name: 'sortie_annuler_form', methods: ['GET'])]
+    public function annulerForm(Sortie $sortie)
     {
         if ($this->getUser() !== $sortie->getOrganisateur() && !$this->isGranted('ROLE_ADMIN')) {
             throw $this->createAccessDeniedException("Vous n'avez pas le droit d'annuler cette sortie");
         }
 
-        $motif = $request->request->get('motifAnnulation');
-        if (!$motif) {
-            $this->addFlash('danger', "Vous devez indiquer un motif d'annulation.");
+        return $this->render('sortie/annuler_form.html.twig', [
+            'sortie' => $sortie
+        ]);
+    }
+
+// Traite la soumission du formulaire d'annulation avec motif
+    #[Route('/sortie/{id}/annuler', name: 'sortie_annuler_form', methods: ['GET', 'POST'])]
+    public function annuler(Sortie $sortie, Request $request, EntityManagerInterface $em)
+    {
+        if ($this->getUser() !== $sortie->getOrganisateur() && !$this->isGranted('ROLE_ADMIN')) {
+            throw $this->createAccessDeniedException("Vous n'avez pas le droit d'annuler cette sortie");
+        }
+
+        $form = $this->createForm(AnnulationSortieForm::class, $sortie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sortie->setEtat(Etat::ANNULEE);
+            $em->flush();
+
+            $this->addFlash('success', 'La sortie a bien été annulée.');
             return $this->redirectToRoute('main_home');
         }
 
-        $sortie->setMotifAnnulation($motif);
-        $sortie->setEtat(Etat::ANNULEE);
-        $em->flush();
-
-        $this->addFlash('success', "La sortie a bien été annulée.");
-        return $this->redirectToRoute('main_home');
+        return $this->render('sortie/annuler_form.html.twig', [
+            'form' => $form->createView(),
+            'sortie' => $sortie
+        ]);
     }
-
 }
---
+++ src/Form/AnnulationSortieForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnulationSortieForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('motifAnnulation', TextareaType::class, [
+                'label' => 'Motif d\'annulation',
+                'required' => true,
+                'attr' => ['rows' => 4]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
